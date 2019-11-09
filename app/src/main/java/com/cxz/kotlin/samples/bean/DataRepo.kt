424307adc716e33e87200263b86d7e1580fb61fb
chenxz <563859095@qq.com>
2019-11-09 17:14:01 +0800

    fix

--- app/src/main/java/com/cxz/kotlin/samples/bean/DataRepo.kt
+++ app/src/main/java/com/cxz/kotlin/samples/bean/DataRepo.kt
@@ -16,19 +16,19 @@ import com.squareup.moshi.Json
 //)
 
 data class HttpResult<T>(
-    @Json(name = "data") val data: T
+        @Json(name = "data") val data: T
 ) : BaseBean()
 
 //轮播图
 data class Banner(
-    @Json(name = "desc") val desc: String,
-    @Json(name = "id") val id: Int,
-    @Json(name = "imagePath") val imagePath: String,
-    @Json(name = "isVisible") val isVisible: Int,
-    @Json(name = "order") val order: Int,
-    @Json(name = "title") val title: String,
-    @Json(name = "type") val type: Int,
-    @Json(name = "url") val url: String
+        @Json(name = "desc") val desc: String,
+        @Json(name = "id") val id: Int,
+        @Json(name = "imagePath") val imagePath: String,
+        @Json(name = "isVisible") val isVisible: Int,
+        @Json(name = "order") val order: Int,
+        @Json(name = "title") val title: String,
+        @Json(name = "type") val type: Int,
+        @Json(name = "url") val url: String
 )
 
 // 登录数据
