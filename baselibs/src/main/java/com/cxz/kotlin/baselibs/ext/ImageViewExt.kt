4eddb5766766750ad7686ce094460d84b4f54037
chenxz <563859095@qq.com>
2021-04-06 11:47:52 +0800

    add ImageViewExt

--- baselibs/src/main/java/com/cxz/kotlin/baselibs/ext/ImageViewExt.kt
+++ baselibs/src/main/java/com/cxz/kotlin/baselibs/ext/ImageViewExt.kt
@@ -1,6 +1,8 @@
 package com.cxz.kotlin.baselibs.ext
 
+import android.content.res.Resources
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.view.ViewGroup
 import android.widget.ImageView
 
@@ -35,4 +37,49 @@ fun ImageView.setScaleImg(bitmap: Bitmap) {
         layoutParams.height = realHeight
         this.layoutParams = layoutParams
     }
+}
+
+/**
+ * 高效加载大型位图
+ * https://developer.android.google.cn/topic/performance/graphics/load-bitmap
+ * imageView.setImageBitmap(decodeSampledBitmapFromResource(resources, R.id.myimage, 100, 100))
+ */
+fun decodeSampledBitmapFromResource(
+    res: Resources,
+    resId: Int,
+    reqWidth: Int,
+    reqHeight: Int
+): Bitmap {
+    // First decode with inJustDecodeBounds=true to check dimensions
+    return BitmapFactory.Options().run {
+        inJustDecodeBounds = true
+        BitmapFactory.decodeResource(res, resId, this)
+
+        // Calculate inSampleSize
+        inSampleSize = calculateInSampleSize(this, reqWidth, reqHeight)
+
+        // Decode bitmap with inSampleSize set
+        inJustDecodeBounds = false
+
+        BitmapFactory.decodeResource(res, resId, this)
+    }
+}
+
+private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+    // Raw height and width of image
+    val (height: Int, width: Int) = options.run { outHeight to outWidth }
+    var inSampleSize = 1
+
+    if (height > reqHeight || width > reqWidth) {
+
+        val halfHeight: Int = height / 2
+        val halfWidth: Int = width / 2
+
+        // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+        // height and width larger than the requested height and width.
+        while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+            inSampleSize *= 2
+        }
+    }
+    return inSampleSize
 }
